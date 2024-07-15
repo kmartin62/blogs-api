df787dd5b848382796adab1b81f8b64029c059c6
Martin Kostadinov <martin.kostadinov@hotmail.com>
2024-07-15 10:39:04 +0200

    chore: add category instead of category id for post

--- dto/post.py
+++ dto/post.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 
+from dto.category import Category
 from dto.tag import Tag
 
 @dataclass
@@ -9,7 +10,7 @@ class Post:
   title: str
   content: str
   published: bool
-  category_id: int
+  category: Category
   tags: List[Tag]
   published_at: datetime
   updated_at: datetime
