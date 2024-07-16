76ce0ea5bebff8dc9231bdc896c4006b9cdd8bb9
Martin Kostadinov <martin.kostadinov@hotmail.com>
2024-07-16 16:09:44 +0200

    chore: add init files and example request for post insertion

--- api/db/model/__init__.py
+++ api/db/model/__init__.py
@@ -1,6 +1,7 @@
 # It's important to keep them in this order
 from .post import Post
+from .category import Category
 from .tag import Tag
 from .user import User
 from .posttags import PostTags
-from .postcategories import PostCategory
+# from .postcategory import PostCategory
